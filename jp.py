7ea5d8abfb58a66d28fbc98114fff8403fa327c8
shinminhsu <shinminhsu@gmail.com>
2019-02-10 15:38:59 +0800

    Export to csv

--- jp.py
+++ jp.py
@@ -5,32 +5,43 @@ import pandas as pd
 
 df = pd.read_csv("/Users/smhsu/Documents/N3.csv")
 jp = df.iloc[:,0]
+audio = []
 
 def auto_dict(item):
 
     try:
+        #change the encdoing of japanese word
         item_encoding = urllib.request.quote(item)
 
+        #build the url of word
         u="https://jisho.org/search/"+item_encoding
         #print(u)
         r=urllib.request.Request(u,headers={'User-Agent':''})
 
+        #read the html code from the url
         data = urllib.request.urlopen(r).read()
-
         t = Selector(text=data)
-        audio_path = 'http:'+t.xpath("//source/@src").get()
+        audio_path = 'http:'+t.xpath("//source/@src").get() #find the mp3 file directory
         a = requests.get(audio_path, allow_redirects=True)
-        open('./audio/%s.mp3' % (item), 'wb').write(a.content)
 
-        print("%s Downloaded") % (item)
+        file_name = item + '.mp3'
+        open('./audio/%s' % (file_name), 'wb').write(a.content) #download the mp3 file
+        audio.append("[sound:" + file_name + "]") #create a name to put into anki
+        print("%s Downloaded" % (item))
+
     except:
-        print("%s Failed") % (item)
+        print("%s Failed" % (item))
+        audio.append("")
 
     return 0
 
 
 for i in jp:
     auto_dict(i)
+
+df['audio'] = audio
+
+df.to_csv(path_or_buf="audio_N3.csv" ,index=False)
 
 
 
